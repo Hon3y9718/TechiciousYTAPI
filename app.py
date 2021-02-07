9b6fc795bcec38ceab1a0e3b42eb53a1f12d480c
Hon3y9718 <39588540+Hon3y9718@users.noreply.github.com>
2021-02-07 13:58:23 +0530

    Update2.0

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, json, jsonify
+from flask_cors import CORS
+from FetchData import SearchFunc, fetchRequiredData, getChannelVideos, SearchotherFunc
+
+app = Flask(__name__)
+CORS(app)
+
+channel_id = 'UC5SMqP_8QsmPDWhNmjgInlg'
+uploads_playlist = 'UU5SMqP_8QsmPDWhNmjgInlg'
+rftString = ['Reflections for Today', 'Reflection for Today']
+QnAString = ['Question and Answer', 'Questions and Answers']
+
+@app.errorhandler(404) 
+def ErrorHandler(e):
+    return f'You found nothing its a 404 Error'
+
+@app.route('/')
+def Home():
+    inforMation = 'In Order to get videos Related to \nQuestion and Answer - /qna\nReflection for Today - /rft \n All Except for QnA and RFT - /others \nFor All Videos - /Everything'
+    return inforMation
+
+@app.route('/qna')
+def FetchQnA():
+    SearchFunc(channel_id, QnAString)
+    Data = fetchRequiredData('fileChannel.json')
+    return jsonify(Data)
+
+@app.route('/rft')
+def FetchRFT():
+    SearchFunc(channel_id, rftString)
+    Data = fetchRequiredData('fileChannel.json')
+    return jsonify(Data)
+
+@app.route('/everything')
+def FetchEverything():
+    getChannelVideos(channel_id)
+    Data = fetchRequiredData('AllVids.json')
+    return jsonify(Data)
+
+@app.route('/others')
+def Other():
+    SearchotherFunc(channel_id)
+    Data = fetchRequiredData('fileChannel.json')
+    return jsonify(Data)
+
+if __name__ == "__main__":
+    app.run()
